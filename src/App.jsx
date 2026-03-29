eaa0c52fc5e6203e924852efe48df7b392e4b374
rashedmern <rashedmolla541@gmail.com>
2026-03-29 23:41:34 +0600

    still working

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+﻿import "./App.css"
+import Navbar from "./components/navbar"
+import Banner from "./components/banner"
+import Clients from "./components/clients"
+import Solutions from "./components/solutions"
+import Growth from "./components/growth"
+import Journey from "./components/journey"
+import MindPower from "./components/mindpower"
+import BetterPart from "./components/betterpart"
+import Operators from "./components/oparetors"
+import QA from "./components/qa"
+import UpFooter from "./components/upfooter"
+import Footer from "./components/footer"
+
+export default function App() {
+  return (
+    <div className="w-full">
+      <Navbar />
+      <Banner />
+      <Clients />
+      <Solutions />
+      <Growth />
+      <Journey />
+      <MindPower />
+      <BetterPart />
+      <Operators />
+      <QA />
+      <UpFooter />
+      <Footer />
+    </div>
+  )
+}
