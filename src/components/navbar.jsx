09a0b65974363c11883a0c2a0627dbcac73e528d
rashedmern <rashedmolla541@gmail.com>
2026-03-31 19:28:23 +0600

    still working on my nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,20 +3,20 @@ import Container from "./Container"
 
 export default function Navbar() {
   return (
-    <div className="w-full py-4 ">
+    <div className="w-full py-4">
       <Container>
-        <div>
-            <div className="flex items-center justify-between mt-4 ">
-            <img src="../Images/Logo.png" alt="Fluxora Logo" className="h-8" />
-        </div>
-        <div>
-          <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <div className="flex items-center justify-between ">
+          <img src="../Images/Logo.png" alt="Fluxora Logo" className=" w-[121px] h-[18px]" />
+          <ul className="flex gap-8">
+            <li className="cursor-pointer hover:text-blue-500">Home</li>
+            <li className="cursor-pointer hover:text-blue-500">Services</li>
+            <li className="cursor-pointer hover:text-blue-500">Projects</li>
+            <li className="cursor-pointer hover:text-blue-500">About</li>
+            <li className="cursor-pointer hover:text-blue-500">Blogs</li>
           </ul>
-        </div>
+          <button className="w-[111px] h-[35px] bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700 transition font-">
+            Get Started
+          </button>
         </div>
       </Container>
     </div>
