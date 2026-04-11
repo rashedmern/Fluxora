d7d7b4c058483550f354b93bf608f5357a8362cd
rashedmern <rashedmolla541@gmail.com>
2026-04-11 12:38:57 +0600

    start working on betterpart

--- src/components/betterpart.jsx
+++ src/components/betterpart.jsx
@@ -5,7 +5,12 @@ export default function BetterPart() {
   return (
     <div className="w-full py-10">
       <Container>
-        <h2>Better Part Section</h2>
+         <h1 className="text-center text-4xl font-poppins font-bold text-[#2C2C2C] mt-12 leading-tight">
+         Insights That Shape Better Products
+        </h1>
+        <p className="text-center text-base font-inter font-normal leading-relaxed tracking-tight text-[#575757] mt-5">
+          Strategic thinking on SaaS design, UX systems, and <br /> product growth — built from real-world execution.
+        </p>
       </Container>
     </div>
   )
