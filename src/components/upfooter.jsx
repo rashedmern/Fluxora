e78ddf2ccf26029f3e45a68c6762753b3b17a676
rashedmern <rashedmolla541@gmail.com>
2026-04-11 12:34:31 +0600

    done with upper-footer

--- src/components/upfooter.jsx
+++ src/components/upfooter.jsx
@@ -15,12 +15,13 @@ export default function UpFooter() {
         <p className="text-center text-[16px] font-inter font-regular leading-[140%] tracking-[-4%] mb-[32px] text-[#575757] mt-5">
           We partner with founders and product teams to design <br /> meaningful, high-performance digital experiences.
         </p>
-          <button
-            className="flex items-center jsutify-center gap-2 bg-[#1C1C1C] text-white rounded-md 
-font-semibold hover:bg-white hover:text-black hover:border  
-transition font-inter text-[14px] mt-10 px-4 py-2"
+         <section className=" flex items-center justify-center">
+           <button
+            className="flex items-center justify-center gap-2 bg-[#1C1C1C] text-white rounded-md 
+font-medium font-inter leading-[136%] hover:bg-white hover:text-black hover:border  
+transition font-inter text-[16px] mt-10 px-4 py-4"
           >
-            Our Story
+            Book a Strategy Call
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 640 640"
@@ -29,6 +30,7 @@ transition font-inter text-[14px] mt-10 px-4 py-2"
               <path d="M566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3C348.8 149.8 348.8 170.1 361.3 182.6L466.7 288L96 288C78.3 288 64 302.3 64 320C64 337.7 78.3 352 96 352L466.7 352L361.3 457.4C348.8 469.9 348.8 490.2 361.3 502.7C373.8 515.2 394.1 515.2 406.6 502.7L566.6 342.7z" />
             </svg>
           </button>
+         </section>
       </Container>
     </div>
   )
